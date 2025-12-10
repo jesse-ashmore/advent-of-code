305eaf85805679db20b658c18bb75cb458bc5283
Jesse Ashmore <ashmjss@amazon.com>
2025-12-10 10:15:26 +0000

    solve(2025-9): 🎄

--- 2025-rs/src/bin/09.rs
+++ 2025-rs/src/bin/09.rs
@@ -6,19 +6,7 @@ use itertools::Itertools;
 advent_of_code::solution!(9);
 
 pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
-    let mut red_tiles = parse_tiles(input);
-    let top_row = red_tiles.iter().map(|(_, y)| y).min().unwrap();
-    let top_left = red_tiles
-        .iter()
-        .filter(|(_, y)| y == top_row)
-        .min_by_key(|(x, _)| x)
-        .unwrap();
-    let start_offset = red_tiles
-        .iter()
-        .find_position(|x| x == &top_left)
-        .unwrap()
-        .0;
-    red_tiles.rotate_left(start_offset);
+    let red_tiles = parse_tiles(input);
     let square_like_ordered = red_tiles
         .pairs()
         .filter(|(a, b)| a.0 != b.0 && a.1 != b.1)
@@ -29,13 +17,10 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
         .first()
         .map(|pair| area_manhattan(*pair));
 
-    let height = red_tiles.iter().map(|(_, y)| y).max().unwrap();
-    let width = red_tiles.iter().map(|(x, _)| x).max().unwrap();
-
     let mut verticals = Vec::new();
     let mut horizontals = Vec::new();
     let mut outside_vert = Vec::new();
-    // let mut outside_horiz = Vec::new();
+    let mut outside_horiz = Vec::new();
 
     // build vectors
     for (a, b) in red_tiles
@@ -61,7 +46,7 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
             .iter()
             .filter(|(_, other_vertical)| {
                 let mid = (vertical.start + vertical.end) / 2;
-                if other_vertical.start <= mid && mid <= other_vertical.end {
+                if other_vertical.start <= mid && mid < other_vertical.end {
                     return true;
                 }
                 false
@@ -75,31 +60,49 @@ pub fn solve(input: &str) -> (Option<u64>, Option<u64>) {
         outside_vert.push((offset_x, (vertical.start + 1..vertical.end - 1)));
     }
 
-    // let largest_inner_rect = square_like_ordered
-    //     .iter()
-    //     .filter(|(a, b)| {
-    //         let top = a.1.min(b.1);
-    //         let right = a.0.max(b.1);
-    //         let bottom = a.1.max(b.1);
-    //         let left = a.0.min(b.0);
+    for (i, (y, horizontal)) in horizontals.iter().enumerate() {
+        let crossed = horizontals[i + 1..]
+            .iter()
+            .filter(|(_, other_horizontal)| {
+                let mid = (horizontal.start + horizontal.end) / 2;
+                if other_horizontal.start <= mid && mid < other_horizontal.end {
+                    return true;
+                }
+                false
+            })
+            .count();
+        let offset_x: u64 = if crossed.rem_euclid(2) == 0 {
+            y + 1
+        } else {
+            y - 1
+        };
+        outside_horiz.push((offset_x, (horizontal.start + 1..horizontal.end - 1)));
+    }
 
-    //         for x in left..=right {
-    //             if perimeter.contains(&(x, top)) || perimeter.contains(&(x, bottom)) {
-    //                 return false;
-    //             };
-    //         }
-    //         for y in top..=bottom {
-    //             if perimeter.contains(&(left, y)) || perimeter.contains(&(right, y)) {
-    //                 return false;
-    //             };
-    //         }
+    let largest_inner_rect = square_like_ordered
+        .iter()
+        .filter(|(a, b)| {
+            let top = a.1.min(b.1);
+            let right = a.0.max(b.0);
+            let bottom = a.1.max(b.1);
+            let left = a.0.min(b.0);
 
-    //         true
-    //     })
-    //     .next()
-    //     .map(|pair| area_manhattan(*pair));
+            if outside_vert.iter().any(|(x, vert)| {
+                (left <= *x && *x <= right) && top <= vert.end && vert.start <= bottom
+            }) {
+                return false;
+            }
+            if outside_horiz.iter().any(|(y, horiz)| {
+                (top <= *y && *y <= bottom) && left <= horiz.end && horiz.start <= right
+            }) {
+                return false;
+            }
 
-    (part_1, None)
+            true
+        })
+        .next()
+        .map(|pair| area_manhattan(*pair));
+    (part_1, largest_inner_rect)
 }
 
 fn get_heading(from: (u64, u64), to: (u64, u64)) -> DirectionAxes {
